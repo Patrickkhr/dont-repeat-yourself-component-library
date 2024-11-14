fdabc969ea0ca0ad0069a4fc06c496b9d7f5b908
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-11-14 14:01:34 +0100

    Finished full page structure, refactoring needed.

--- src/lib/components/pages/AboutUs.svelte
+++ src/lib/components/pages/AboutUs.svelte
@@ -2,6 +2,17 @@
     import { Image } from '$lib/index'
     import Skyline from '$lib/assets/backdrop-skyline.png'
     import Scenery from '$lib/assets/about-us-scenery.jpg'
+
+    import Vibe from '$lib/assets/vibe.png'
+    import Lillet from '$lib/assets/lillet.png'
+    import Pernod from '$lib/assets/pernod.png'
+    import Bacardi from '$lib/assets/bacardi.png'
+    import Hendricks from '$lib/assets/hendricks.png'
+    import Leonista from '$lib/assets/leonista.png'
+    import London from '$lib/assets/london.png'
+    import Absolut from '$lib/assets/absolut.png'
+    import Cola from '$lib/assets/cocacola.png'
+    import Passoa from '$lib/assets/passoa.png'
 </script>
 
 <svelte:head>
@@ -35,6 +46,32 @@
 
 <section id="partners">
   <h2>Partners</h2>
+  <div class="logos">
+    <div class="logos-slide">
+      <img src={Vibe} alt="Vibe Logo">
+      <img src={Lillet} alt="Lillet Logo">
+      <img src={Pernod} alt="Pernod Ricard Logo">
+      <img src={Bacardi} alt="Bacardi Logo">
+      <img src={Hendricks} alt="Hendricks Gin Logo">
+      <img src={Leonista} alt="Leonista Logo">
+      <img src={London} alt="The London Essence Logo">
+      <img src={Absolut} alt="Absolut Vodka Logo">
+      <img src={Cola} alt="Coca Cola Logo">
+      <img src={Passoa} alt="Passoa Logo">
+    </div>
+    <div class="logos-slide">
+      <img src={Vibe} alt="Vibe Logo">
+      <img src={Lillet} alt="Lillet Logo">
+      <img src={Pernod} alt="Pernod Ricard Logo">
+      <img src={Bacardi} alt="Bacardi Logo">
+      <img src={Hendricks} alt="Hendricks Gin Logo">
+      <img src={Leonista} alt="Leonista Logo">
+      <img src={London} alt="The London Essence Logo">
+      <img src={Absolut} alt="Absolut Vodka Logo">
+      <img src={Cola} alt="Coca Cola Logo">
+      <img src={Passoa} alt="Passoa Logo">
+    </div>
+  </div>
 </section>
 
 <section id="getuigenissen">
@@ -46,23 +83,32 @@
 
 
 <section id="information">
-  <h3>Kledingvoorschrift</h3>
-  <p class="initial">Geen strikte kledingvoorschrift, maar alsjeblieft, geen korte broek of zonnebril.</p>
+  <article>
+    <h3>Kledingvoorschrift</h3>
+    <p class="initial">Geen strikte kledingvoorschrift, maar alsjeblieft, geen korte broek of zonnebril.</p>
+  </article>
 
-  <h3>Reserveringen</h3>
-  <p class="initial">Kies een balk op de pagina Bars, en kijk welke promotie ze beschikbaar hebben!</p>
+  <article>
+    <h3>Reserveringen</h3>
+    <p class="initial">Kies een balk op de pagina Bars, en kijk welke promotie ze beschikbaar hebben!</p>
+  </article>
 
-  <h3>Evenementen</h3>
-  <p class="initial">Koop tickets via onze evenementenpagina, en geniet van de lekkerste cocktails van Amsterdam.</p>
+  <article>
+    <h3>Evenementen</h3>
+    <p class="initial">Koop tickets via onze evenementenpagina, en geniet van de lekkerste cocktails van Amsterdam.</p>
+  </article>
 
-  <h3>Vragen</h3>
-  <p class="initial">Stel gerust al uw vragen, of neem contact met ons op met klachten of complimenten.</p>
+  <article>
+    <h3>Vragen</h3>
+    <p class="initial">Stel gerust al uw vragen, of neem contact met ons op met klachten of complimenten.</p>
+  </article>
 </section>
 
 <style>
   /* ##### Global Styling ##### */
   * {
     text-transform: uppercase;
+    line-height: 1.4;
   }
   .initial {
     text-transform: initial;
@@ -78,7 +124,7 @@
   h1, h2 {
     color: var(--accent2-primary);
     font-size: var(--fs-2xl);
-    margin-bottom: 4rem;
+    margin-bottom: 3rem;
   }
   h3 {
     color: var(--txt-dark-clr);
@@ -117,6 +163,7 @@
     text-align: center;
 
     & span {
+      margin-top: 2rem;
       color: var(--txt-secondary-clr);
     }
     & img {
@@ -125,12 +172,50 @@
     }
   }
   #partners {
+    height: 50vh;
+    align-items: inherit;
+    text-align: center;
+    background: var(--accent2-primary);
+
     & h2 {
       color: var(--txt-dark-clr);
     }
   }
-  #partners,
   #information {
     background: var(--accent2-primary);
+    display: grid;
+    grid-template-columns: 40% 40%;
+
+    & article {
+      justify-self: center;
+
+      & p {
+        max-width: 50ch;
+        letter-spacing: 2px;
+      }
+    }
+  }
+  /* ##### Infinite Carrousel ##### */
+  .logos {
+    white-space: nowrap;
+  }
+  .logos:hover .logos-slide {
+    animation-play-state: paused;
+  }
+  .logos-slide {
+    display: inline-block;
+    animation: 35s slide infinite linear;
+  }
+  .logos-slide img {
+    height: 50px;
+    margin: 0 40px;
+  }
+  @keyframes slide {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(-100%);
+    }
   }
 </style>
